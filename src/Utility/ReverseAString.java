554b1ce97e468b2feb45e104ee6b33f33d235065
Olga Andratova <orgaeva00@gmail.com>
2020-08-05 20:55:57 -0400

    add GitFun.java

---
+++ src/Utility/ReverseAString.java
@@ -0,0 +1,20 @@
+package Utility;
+
+public class ReverseAString {
+
+    //Java ---> avaJ
+    public static void main(String[] args) {
+        String reverse = "";
+
+     //   for (int i = str.length()-1;i >= 0; i --){
+        //    char ch = str.charAt(i);
+       //     reverse
+        }
+
+
+
+
+
+    }
+
+
